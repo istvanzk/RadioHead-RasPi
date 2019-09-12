db26034744b39ec0498ecf12fffbd3d30e08fee9
Istvan Z. Kovacs <istvanzk@gmail.com>
2019-09-12 22:22:09 +0200

    Force len=40 in readFifo

--- RH_RF22.cpp
+++ RH_RF22.cpp
@@ -327,7 +327,7 @@ void RH_RF22::readFifo()
 {
     uint8_t len = spiRead(RH_RF22_REG_4B_RECEIVED_PACKET_LENGTH);
     _rxBufValid = false;
-    len = 60;
+    len = 40;
     // May have already read one or more fragments
     // Get any remaining unread octets, based on the expected length
     // First make sure we dont overflow the buffer in the case of a stupid length
