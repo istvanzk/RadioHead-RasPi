3fa4aeab2aef7086192f2a52414bbafb35fdddcb
Istvan Z. Kovacs <istvanzk@gmail.com>
2020-07-16 22:41:29 +0200

    Add include from RHutil_rf22b when bcm2835 is used

--- RadioHead.h
+++ RadioHead.h
@@ -1490,6 +1490,8 @@ these examples and explanations and extend them to suit your needs.
  #define PROGMEM
  #if (__has_include (<pigpio.h>))
   #include <RHutil_pigpio/RasPi.h>
+ #elif (__has_include (<bcm2835.h>))
+  #include <RHutil_rf22b/RasPi.h> 
  #else
   #include <RHutil/RasPi.h>
  #endif
